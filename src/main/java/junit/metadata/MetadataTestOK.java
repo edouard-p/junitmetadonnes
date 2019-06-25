f7970e4d0b3e133d0f300416eaf262fbc8d02995
edouard-p <51782288+edouard-p@users.noreply.github.com>
2019-06-25 17:03:55 +0200

    Update MetadataTestOK.java

--- src/main/java/junit/metadata/MetadataTestOK.java
+++ src/main/java/junit/metadata/MetadataTestOK.java
@@ -15,7 +15,6 @@ public class MetadataTestOK {
     }
 
     @Test
-    @TFMetadata( key = "solo_KEY")
     public void keyOnly(){
         int a = 2;
         int b = 2;
@@ -32,7 +31,6 @@ public class MetadataTestOK {
 
     @Test
     @TFMetadata( key = "first_KEY", value = {"first_VALUE"})
-    @TFMetadata( key = "solo")
     @TFMetadata( key = "list", value = {"first_item", "second_item", "third,_item"})
     public void multiAnnot(){
         int a = 2;
@@ -42,7 +40,6 @@ public class MetadataTestOK {
 
     @Test
     @TFMetadata( key = "1111", value = {"22222"})
-    @TFMetadata( key = "3333")
     @TFMetadata( key = "5555", value = {"6666", "7777", "8888"})
     public void numbersEverywhere(){
         int a = 2;
@@ -52,7 +49,6 @@ public class MetadataTestOK {
 
     @Test
     @TFMetadata( key = "first_K-E.Y", value = {"first_VALUE"})
-    @TFMetadata( key = "se.con-d_KEY")
     @TFMetadata( key = "mu.l-ti_Key", value = {"one", "two", "three"})
     @TFMetadata( key = "first", value = {"fi/r-s.t_VALUE"})
     @TFMetadata( key = "second", value = {"o/ne", "t.wo", "t-hr_ee"})
